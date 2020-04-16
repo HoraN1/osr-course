bb6a073cfa2b7157a73909b3151795e3591f8f3a
HoraN1 <horacesunhe@gmail.com>
2020-04-16 20:08:07 +0800

    Fix docker-run.sh bug

--- docker-run.sh
+++ docker-run.sh
@@ -14,4 +14,4 @@ docker run --gpus all -it \
     -v $HOME/.Xauthority:/root/.Xauthority \
     -v ${PWD}/catkin_ws/src/osr_course_pkgs:/home/cri_osr/catkin_ws/src/osr_course_pkgs \
     -w /home/cri_osr/catkin_ws/src \
-    crigroup/osr-course
+    horasun/osr-course
