a6779eed0b5c8570381aa84f67155c9340cc2586
HoraN1 <horacesunhe@gmail.com>
2020-04-16 16:32:26 +0800

    Update docker-run.sh to get colorful bash.

--- docker-run.sh
+++ docker-run.sh
@@ -4,6 +4,7 @@ docker run --gpus all -it \
     --net=host \
     -e DISPLAY=$DISPLAY \
     -e QT_X11_NO_MITSHM=1 \
+    -e TERM=xterm-256color \
     -v $HOME/.Xauthority:/root/.Xauthority \
     -v ${PWD}/catkin_ws/src/osr_course_pkgs:/home/cri_osr/catkin_ws/src/osr_course_pkgs \
     -w /home/cri_osr/catkin_ws/src \
