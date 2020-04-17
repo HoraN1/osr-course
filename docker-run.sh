db305b5efa094bb4762f951859f63f6f709d0e6a
HoraN1 <horacesunhe@gmail.com>
2020-04-17 16:50:16 +0800

    Update bash scripits same as ROS-GUI.

--- docker-run.sh
+++ docker-run.sh
@@ -6,13 +6,18 @@
 # Description:
 #     Docker run command scripts: docker run ...
 
+# Looking for catkin workspace, if not found, make one.
+dir="./catkin_ws/wrc"
+if [ ! -d "$dir" ]
+then
+    mkdir -p catkin_ws/src
+fi
+
 docker run --gpus all -it \
     --net=host \
     -e DISPLAY=$DISPLAY \
     -e QT_X11_NO_MITSHM=1 \
     -e TERM=xterm-256color \
     -v $HOME/.Xauthority:/root/.Xauthority \
-    -v ${PWD}/catkin_ws:/home/cri_osr/catkin_ws \
-    -v $PWD/scripts-container:/home/cri_osr/scripts-container \
-    -w /home/cri_osr/catkin_ws/src \
+    -v $PWD/catkin_ws:~/catkin_ws \
     horasun/osr-course
--- scripts-container/initialize-workspace.sh
+++ scripts-container/initialize-workspace.sh
@@ -6,12 +6,14 @@
 # Description:
 #     Initialize ros workspace
 
-source /opt/ros/kinetic/setup.bash
 # Create catkin workspace
-cd ~/catkin_ws/src && catkin_init_workspace
+source /opt/ros/kinetic/setup.bash
+
 # Build workspace
 cd ~/catkin_ws && catkin_make
 source ~/catkin_ws/devel/setup.bash
+
 # Configure .bashrc
+echo "# Configure your .bashrc" >> ~/.bashrc
 echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
 source ~/.bashrc
